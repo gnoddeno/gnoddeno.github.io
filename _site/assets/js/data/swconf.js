2db8f687d4b4c0ac1ce6ef18e240eb87ec274423
Gnoddeno <hanran34@dgu.ac.kr>
2024-12-23 14:06:37 +0900

    test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734928002',resources: [
+    cacheName: 'chirpy-1734930377',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
