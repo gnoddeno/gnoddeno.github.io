64b743914566a0bfb1330557192897435d17d350
Gnoddeno <hanran34@dgu.ac.kr>
2024-12-23 13:27:06 +0900

    init settings

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734926401',resources: [
+    cacheName: 'chirpy-1734928002',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
