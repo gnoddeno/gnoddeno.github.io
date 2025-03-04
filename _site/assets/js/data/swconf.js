d7d788df1affa5a9513571998e2e1d2018282631
gnoddeno <hanran34@dgu.ac.kr>
2025-03-04 19:55:52 +0900

    검색엔진 노출

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734939701',resources: [
+    cacheName: 'chirpy-1734944835',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
